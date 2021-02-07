dc91b45a6c37b533f337aaacedc2d6f655c9e11f
mahmudshumit <rahmanshumit@gmail.com>
2021-02-07 19:33:15 +0600

    finished

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const searchBtn = document.getElementById('search-btn');
-const mealList = document.getElementById('meal');
-const mealDetailsContent = document.querySelector('.meal-details-content');
-const detailBtn = document.getElementById('detail-btn');
+const searchBtn = document.getElementById("search-btn");
+const mealList = document.getElementById("meal");
+const mealDetailsContent = document.querySelector(".meal-details-content");
+const detailBtn = document.getElementById("detail-btn");
 
-
-searchBtn.addEventListener('click', getMealList);
-mealList.addEventListener('click', getMealDetail);
-deatailBtn.addEventListener('click', () => {
-    mealDetailsContent.parentElement.classList.remove('showDetail');
+searchBtn.addEventListener("click", getMealList);
+mealList.addEventListener("click", getMealDetail);
+deatailBtn.addEventListener("click", () => {
+  mealDetailsContent.parentElement.classList.remove("showDetail");
 });
 
-
 function getMealList() {
-    let searchInputTxt = document.getElementById('search-input').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
-        .then(response => response.json())
-        .then(data => {
-            let html = "";
-            if (data.meals) {
-                data.meals.forEach(meal => {
-                    html += `
+  let searchInputTxt = document.getElementById("search-input").value;
+  fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let html = "";
+      if (data.meals) {
+        data.meals.forEach((meal) => {
+          html += `
                     <div class = "meal-item" data-id = "${meal.idMeal}">
                         <div class = "meal-img">
                             <img src = "${meal.strMealThumb}" alt = "food">
@@ -30,34 +30,31 @@ function getMealList() {
                         </div>
                     </div>
                 `;
-                });
-                mealList.classList.remove('notFound');
-            } else {
-                html = "Sorry, we didn't find any meal!";
-                mealList.classList.add('notFound');
-            }
-
-            mealList.innerHTML = html;
         });
+        mealList.classList.remove("notFound");
+      } else {
+        html = "Sorry, we didn't find any meal!";
+        mealList.classList.add("notFound");
+      }
+
+      mealList.innerHTML = html;
+    });
 }
-
-
-
 function getMealDetail(e) {
-    e.preventDefault();
-    if (e.target.classList.contains('detail-btn')) {
-        let mealItem = e.target.parentElement.parentElement;
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
-            .then(response => response.json())
-            .then(data => mealInfo(data.meals));
-    }
+  e.preventDefault();
+  if (e.target.classList.contains("detail-btn")) {
+    let mealItem = e.target.parentElement.parentElement;
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
+    )
+      .then((response) => response.json())
+      .then((data) => mealInfo(data.meals));
+  }
 }
-
-
 function mealInfo(meal) {
-    console.log(meal);
-    meal = meal[0];
-    let html = `
+  console.log(meal);
+  meal = meal[0];
+  let html = `
         <h2 class = detail-title">${meal.strMeal}</h2>
         <p class = "detail-category">${meal.strCategory}</p>
         <div class = "detail-instruct">
@@ -69,6 +66,6 @@ function mealInfo(meal) {
         </div>
         
     `;
-    mealDetailsContent.innerHTML = html;
-    mealDetailsContent.parentElement.classList.add('showDetail');
+  mealDetailsContent.innerHTML = html;
+  mealDetailsContent.parentElement.classList.add("showDetail");
 }
